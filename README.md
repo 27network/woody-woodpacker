6c8962dc52e7eee47b1e11c2ba898b1a5629410e
xtrm <oss@xtrm.me>
2025-02-13 21:23:19 +0100

    🔨 chore: initial commit
    Signed-off-by: xtrm <oss@xtrm.me>

---
+++ README.md
@@ -0,0 +1,7 @@
+# [woody-woodpacker](https://projects.intra.42.fr/projects/42cursus-woody-woodpacker)
+
+> A logical follow-up project on nm/otools that is designed to modify the headers of a ELF64 file. The goal here is to add a chunk of code and hide a non-stripped part of a file.
+
+## License
+
+This project is licensed under the [ISC License](./LICENSE).
---
+++ src/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 21:22:53 by kiroussa          #+#    #+#             */
+/*   Updated: 2025/02/13 21:22:53 by kiroussa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ww/core.h>
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	ft_dprintf(2, "Hello, woody-woodpacker!\n");
+	return (0);
+}
